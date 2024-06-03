be8603b8e2ba9686bff73a6f2b15b4e19eba1799
Scott Lyons <scottalyons@gmail.com>
2024-06-03 09:19:58 -0700

    Javascript example plugin

--- js_wasm_udf/src/index.js
+++ js_wasm_udf/src/index.js
@@ -2,16 +2,17 @@ var wasmudf = require("./wasmudf_pb.js")
 var literal = require("./literal_pb.js")
 
 export function add_two() {
-    const inputBytes = Host.input()
-    var args = wasmudf.UDFArgs.deserializeBinary(bytes);
+    const inputBytes = Host.inputBytes()
+    var args = wasmudf.UDFArgs.deserializeBinary(inputBytes);
     var argsList = args.getArgsList()
     if (argsList.length == 2) {
         var first_arg = argsList[0];
         var second_arg = argsList[1];
         if (first_arg.hasInteger() && second_arg.hasInteger()) {
             var res = new literal.Literal();
-            res.setInteger(first_arg + second_arg);
-            Host.output(res.serializeBinary())
+            res.setInteger(first_arg.getInteger() + second_arg.getInteger());
+            var newBytes = res.serializeBinary();
+            Host.outputBytes(new Uint8Array(newBytes).buffer);
             return 0;
         } else {
             Host.outputString("Both arguments should be Integers");
